cdf0025ff12267ec73a523c618ed24abc7a656a5
Gideon Buckwalter <gideon.buckwalter@gmail.com>
2017-04-20 17:27:17 -0400

    Restructure to use pointers instead of indices, and put everything in a class

--- heapsort.hpp
+++ heapsort.hpp
@@ -4,42 +4,68 @@
 #include <functional>
 
 
-
-// HELPER FUNCTIONS //
-
-int parent(int i) { return (i + 1)/2; }
-int childL(int i) { return (i + 1)*2; }
-int childR(int i) { return (i + 1)*2 + 1; }
-
-bool hasChildL(int i, int size) { return childL(i) < size; }
-bool hasChildR(int i, int size) { return childR(i) < size; }
-
-bool isLeaf(int i, int size)
-{ return !hasChildL(i, size) && !hasChildR(i, size); } // Simplification?
-
-
-
-// MAIN FUNCTIONS //
-
-// Main heapsort function.
 template <class T>
-void heapsort(T arr[], int size, std::function<double (T)> key)
+class HeapSort
 {
-	heapify(arr, size, key);
-	extractAll(arr, size, key);
-}
+private:
+	T* head;
+	int size;
+	std::function<double (T)> key;
 
-template <class T>
-void heapify(T arr[], int size, std::function<double (T)> key)
-{
+	// HELPER FUNCTIONS //
 
-}
+	// Returns the index of the given pointer relative to 'head'.
+	// If ptr == head, return 0
+	int index(T* ptr) { return static_cast<int>(ptr - head); }
 
-template <class T>
-void extractAll(T heap[], int size, std::function<double (T)> key)
-{
+	// Return pointers to parents and children of given pointers.
+	T* parent(T* ptr) { return head + (index(ptr) - 1)/2; }
+	T* childL(T* ptr) { return head + index(ptr)*2 + 1; }
+	T* childR(T* ptr) { return head + index(ptr)*2 + 2; }
 
-}
+	// Check for existence of children/parents of a given pointer.
+	bool hasParent(T* ptr) { return parent(ptr) > head; }
+	bool hasChildL(T* ptr) { return childL(ptr) <= head + size; }
+	bool hasChildR(T* ptr) { return childR(ptr) <= head + size; }
+
+	// Tests if a given pointer points to a leaf node.
+	// NOTE: If node had no left node, it cannot have a right node either.
+	bool isLeaf(T* ptr)
+	{ return !hasChildL(ptr); }
+
+
+
+	// MAIN FUNCTIONS //
+
+	// Main heapsort function.
+	HeapSort(T array[], int size, std::function<double (T)> key)
+	{
+		this->head = array
+		this->size = size;
+		this->key = key;
+	}
+
+	void go()
+	{
+		heapify();
+		extractAll();
+	}
+
+	void heapify()
+	{
+
+	}
+
+	void extractAll()
+	{
+
+	}
+
+	void percolateDown(T* start, T* end)
+	{
+
+	}
+};
 
 
 #endif // HEAPSORT
